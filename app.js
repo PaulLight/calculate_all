630933e0f364a40f97bcc7b639f01252fb35fdaf
paul_light <dctr.waker@gmail.com>
2019-07-10 17:27:18 +0200

    html correct

---
+++ app.js
@@ -0,0 +1,19 @@
+//Listen fo ubmit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+//Calculate results
+
+function calculateResults(e){
+    //UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculateResults = parseFloat(interest.value)/100/12;
+
+    e.preventDefault();
+}
+
