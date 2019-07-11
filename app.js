940c09ba2820847ac3694b17cd19dc17905beca7
paul_light <dctr.waker@gmail.com>
2019-07-11 06:11:12 +0200

    Adding calculate and error message

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ document.getElementById('loan-form').addEventListener('submit', function(e){
     document.getElementById('loading').style.display = 'block';
 
     setTimeout(calculateResults, 1000);
-
     e.preventDefault();
 });
 //Calculate results
