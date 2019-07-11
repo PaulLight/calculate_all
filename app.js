47e037f6f855426c24d3904306f3972e5f09ddbe
paul_light <dctr.waker@gmail.com>
2019-07-12 00:14:11 +0200

    GDP Calculation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Listen fo submit
+//Listen for submit
 document.getElementById('loan-form').addEventListener('submit', function(e){
     //Hide results
     document.getElementById('results').style.display = 'none';
@@ -7,77 +7,79 @@ document.getElementById('loan-form').addEventListener('submit', function(e){
     document.getElementById('loading').style.display = 'block';
 
     setTimeout(calculateResults, 1000);
-
+    
     e.preventDefault();
 });
-//Calculate results
 
-function calculateResults(e){
+//Calculate Results
+
+function calculateResults(){
     //UI Vars
-    const amount = document.getElementById('amount');
-    const interest = document.getElementById('interest');
-    const years = document.getElementById('years');
-    const monthlyPayment = document.getElementById('monthly-payment');
-    const totalPayment = document.getElementById('total-payment');
-    const totalInterest = document.getElementById('total-interest');
 
-    const principal = parseFloat(amount.value);
-    const calculatedInterest = parseFloat(interest.value)/100/12;
-    const calculatedPayments = parseFloat(years.value)*12;
+    const amountconsumption = document.getElementById('consumption');
+    const spending = document.getElementById('spending');
+    const investment = document.getElementById('investment');
+    const total_exports = document.getElementById('total_exports');
+    const total_import = document.getElementById('total_import');
+    const LastYearGDP = document.getElementById('LastYearGDP');
+    const gdp_spending = document.getElementById('gdp_spending')
+    const growth_rate = document.getElementById('growth_rate')
+    const net_export = document.getElementById('net_export');
 
-    //Compute mnthly payment
-    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principal*x*calculatedInterest)/(x-1); 
     
-    if(isFinite(monthly)){
-        monthlyPayment.value = monthly.toFixed(2);
-        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    const nexport = parseFloat(total_exports.value) - parseFloat(total_import.value);
+    const spent = parseFloat(nexport.value) + parseFloat(spending.value) + parseFloat(investment.value) + parseFloat(amountconsumption.value);
+    const x = parseFloat(spent.value) - parseFloat(LastYearGDP.value);
+    const grate = x / parseFloat(LastYearGDP.value) *100;
+
+    if (isFinite(spent)){
+
+        growth_rate.value = grate.toFixed(2);
+        gdp_spending.value = spent.toFixed(2);
+        net_export.value = nexport.toFixed(2);
 
         //Show results
-        document.getElementById('results').style.display = 'block';
+        document.getElementById('results').style.display = ' block';
 
         //Hide loader
-        document.getElementById('loading').style.display = 'none';
+        document.getElementById('loading').style.alignContent.display = 'none';
+
     }else{
-       showError('Please check your numbers');
+        showError('Please check your numbers');
     }
+
 }
 
-//Show error 
-
+//Show error
 function showError(error){
-
-    //Hide results
+    // Hide results
     document.getElementById('results').style.display = 'none';
-
-    //Hide loader
+    
+    // Hide loader
     document.getElementById('loading').style.display = 'none';
-
-    //Create div
+  
+    // Create a div
     const errorDiv = document.createElement('div');
-
-    //Get elements
+  
+    // Get elements
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
-
-    //Add class
+  
+    // Add class
     errorDiv.className = 'alert alert-danger';
-
-    //Create text node and append to div
+  
+    // Create text node and append to div
     errorDiv.appendChild(document.createTextNode(error));
-
-    //Insert error about heading
+  
+    // Insert error above heading
     card.insertBefore(errorDiv, heading);
-
-    //Clear error after 3 seconds
+  
+    // Clear error after 3 seconds
     setTimeout(clearError, 3000);
+  }
 
-}
 //Clear error
 function clearError(){
     document.querySelector('.alert').remove();
 }
-
-
-
